40b791bbf2369d596ffc2b8e9b3ba5450229c56e
vlad526 <vlad.kar765@gmail.com>
2026-05-17 17:09:19 +0300

    1

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -12,7 +12,6 @@ export const configs = {
 
     MONGO_URI: process.env.MONGO_URI,
 
-
     JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET,
     JWT_ACCESS_EXPIRATION: process.env.JWT_ACCESS_EXPIRATION,
     JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET,
